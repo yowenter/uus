98259f6f3d5f8ac31b1e151f6a9eac04a901d28e
ubuntu <ubuntu@example.com>
2017-10-18 19:19:02 +0800

    >fix

--- supervisord.sh
+++ supervisord.sh
@@ -11,18 +11,10 @@ stderr_logfile=/dev/stderr
 stderr_logfile_maxbytes=0
 stdout_logfile=/dev/stdout
 stdout_logfile_maxbytes=0
-[program:app]
-command=python /opt/lenovo/UnifiedService/webroot/bin/ServerStart.pyc
-autorestart=true
-startsecs=0
-stderr_logfile=/dev/stderr
-stderr_logfile_maxbytes=0
-stdout_logfile=/dev/stdout
-stdout_logfile_maxbytes=0
 EOT
 echo "Init configuration."
 /etc/init.d/lxiserver start 
 python /opt/lenovo/UnifiedService/render_conf.py
 echo "Sync db"
-#python /opt/lenovo/UnifiedService/webroot/bin/keepDBupdate.pyc
+python /opt/lenovo/UnifiedService/webroot/bin/keepDBupdate.pyc
 exec supervisord
